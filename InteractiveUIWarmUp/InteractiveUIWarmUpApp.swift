76454738a26800faef6f6d82aebaea484f77898d
Samshi Chinthireddy <samshi.chinthireddy@gmail.com>
2025-07-24 09:08:08 -0700

    Initial Commit

---
+++ InteractiveUIWarmUp/InteractiveUIWarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIWarmUpApp.swift
+//  InteractiveUIWarmUp
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIWarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveUIWarmUpTests/InteractiveUIWarmUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIWarmUpTests.swift
+//  InteractiveUIWarmUpTests
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Testing
+@testable import InteractiveUIWarmUp
+
+struct InteractiveUIWarmUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
